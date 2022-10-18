5294253fe84abccce059a9009d1eabe171fe1522
greenlem <65173994+greenlem@users.noreply.github.com>
2022-10-18 15:49:23 -0400

    Part 3 Game Objects Images complete

--- game.js
+++ game.js
@@ -1,10 +1,11 @@
-window.onload = function() {
-    var config = {
-        width: 800,
-        height: 600,
-        backgroundColor: 0x000000,
-        scene: [Scene1, Scene2]
-    }
+var config = {
+    width: 256,
+    height: 272,
+    backgroundColor: 0x000000,
+    scene: [Scene1, Scene2],
+    pixelArt: true
+}
 
+window.onload = function() {
     var game = new Phaser.Game(config);
 }
