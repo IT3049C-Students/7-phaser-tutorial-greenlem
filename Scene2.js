2c7045926d5373d2c45a629b94a57c1ac271b945
greenlem <65173994+greenlem@users.noreply.github.com>
2022-10-19 14:20:57 -0400

    Part 8 custom classes done

--- Scene2.js
+++ Scene2.js
@@ -24,6 +24,7 @@ class Scene2 extends Phaser.Scene{
         this.cursorKeys = this.input.keyboard.createCursorKeys();
         this.player.setCollideWorldBounds(true);
         this.spacebar = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+        this.projectiles = this.add.group();
 
         this.ship1.setInteractive();
         this.ship2.setInteractive();
@@ -81,7 +82,11 @@ class Scene2 extends Phaser.Scene{
         this.movePlayerManager();
 
         if (Phaser.Input.Keyboard.JustDown(this.spacebar)){
-            console.log("Fire!");
+            this.shootBeam();
+        }
+        for(var i = 0; i < this.projectiles.getChildren().length; i++){
+            var beam = this.projectiles.getChildren()[i];
+            beam.update();
         }
     }
 
@@ -97,5 +102,9 @@ class Scene2 extends Phaser.Scene{
         }else if(this.cursorKeys.down.isDown){
             this.player.setelocityY(gameSettings.playerSpeed);
         }
+    }
+
+    shootBeam(){
+        var beam = new Beam(this);
     }
 }
